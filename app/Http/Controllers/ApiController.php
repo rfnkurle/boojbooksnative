324be2ab97d15ee5617f438c86abd2f0b23f53c2
robiwankenobi <rfnkurle@gmail.com>
2019-10-23 13:37:37 -0700

    added find all books and find book by id

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,7 +10,9 @@ class ApiController extends Controller
 //controller methods need to match api routes
 {
     public function getAllBooks() {
-        // 
+        $books = Book::get()->toJson(JSON_PRETTY_PRINT);
+    return response($books, 200);
+    // will serialize the object data return by eloquent into  JSON. The JSON is returned with the response code 200.
       }
   
       public function createBook(Request $request) {
@@ -26,8 +28,19 @@ class ApiController extends Controller
     ], 201);
       }
   
+
       public function getBook($id) {
-        // 
+        //   checks if a student record with the given id exists. If it does, it queries 
+        // the database using eloquent to return the record with matching id in JSON with 200 as the response code.
+       
+        if (Book::where('id', $id)->exists()) {
+            $book = Book::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($book, 200);
+          } else {
+            return response()->json([
+              "message" => "book not found"
+            ], 404);
+          }
       }
   
       public function updateBook(Request $request, $id) {
