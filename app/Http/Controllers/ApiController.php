8228104527421da6b6fca3b707327318d0b05374
robiwankenobi <rfnkurle@gmail.com>
2019-10-23 13:55:56 -0700

    Added Update and Delete Routes, time for a break

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -44,11 +44,38 @@ class ApiController extends Controller
       }
   
       public function updateBook(Request $request, $id) {
-        // 
+        if (Book::where('id', $id)->exists()) {
+            $book = Book::find($id);
+            $book->title = is_null($request->title) ? $book->title : $request->title;
+            $book->author = is_null($request->author) ? $book->author : $request->author;
+            $book->description = is_null($request->description) ? $book->description : $request->description;
+            $book->publication = is_null($request->publication) ? $book->publication : $request->publication;
+            $book->save();
+    
+            return response()->json([
+                "message" => "updated successfully"
+            ], 200);
+            } else {
+            return response()->json([
+                "message" => "book not found"
+            ], 404);
+            
+        }
       }
   
       public function deleteBook ($id) {
-        // 
+        if(Book::where('id', $id)->exists()) {
+            $book = Book::find($id);
+            $book->delete();
+    
+            return response()->json([
+              "message" => "book infromation deleted"
+            ], 202);
+          } else {
+            return response()->json([
+              "message" => "Book not found"
+            ], 404);
+          }
       }
     
 }
